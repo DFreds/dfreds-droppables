a4771e2f379e4dc21d5ae39404323ce0bb7112f0
Derek Fredrickson <dfreds08@gmail.com>
2021-04-13 20:29:09 -0400

    Add dropping journal entry folders onto canvas

--- scripts/droppable.js
+++ scripts/droppable.js
@@ -1,23 +1,35 @@
 import log from './logger.js';
 
 export default class Droppable {
+  /**
+   * This function is called when something is dropped onto the canvas. If the
+   * item dropped onto the canvas is a folder, it is handled here. Otherwise,
+   * the original wrapper function is used.
+   *
+   * @param {fn} wrapper - The original onDrop function
+   * @param  {...any} args - Any arguments provided with the original onDrop function
+   */
   onCanvasDrop(wrapper, ...args) {
-    const [event] = args;
     try {
+      const [event] = args;
       const data = this._getDataFromEvent(event);
 
       if (data.type !== 'Folder') {
-        return wrapper(...args);
+        wrapper(...args);
+        return;
       }
 
-      // TODO handle other types of data
       if (data.entity === 'Actor') {
         this._handleActorFolder(data, event);
+      } else if (data.entity === 'JournalEntry') {
+        this._handleJournalFolder(data, event);
       } else {
         wrapper(...args);
+        return;
       }
     } catch (error) {
       wrapper(...args);
+      return;
     }
   }
 
@@ -26,49 +38,58 @@ export default class Droppable {
   }
 
   async _handleActorFolder(data, event) {
-      const folder = game.folders.get(data.id);
-      const actors = folder.content;
-      const topLeft = this._getTopLeft(event);
+    const folder = game.folders.get(data.id);
+    const actors = folder.content;
+    const topLeft = this._getTopLeft(event);
 
-      if (actors.length === 0) return;
+    if (actors.length === 0) return;
 
-      for (let actor of actors) {
-        await this._dropActor(actor, this._convertEventInfoToTokenData(topLeft[0], topLeft[1], event.altKey));
-      }
-  }
-
-  _getTopLeft(event) {
-      const t = canvas.tokens.worldTransform,
-        tx = (event.clientX - t.tx) / canvas.stage.scale.x,
-        ty = (event.clientY - t.ty) / canvas.stage.scale.y;
-
-      return canvas.grid.getTopLeft(tx, ty);
-  }
-
-  _convertEventInfoToTokenData(xPosition, yPosition, isHidden) {
-    return {
-      x: xPosition,
-      y: yPosition,
-      hidden: isHidden
+    for (let actor of actors) {
+      await this._dropActor(actor, topLeft[0], topLeft[1], event.altKey);
     }
   }
 
   // TODO dropping in different ways
   // https://github.com/League-of-Foundry-Developers/fvtt-module-furnace/blob/07bddbb1324a92f7d48513128daef62af57c25cb/QoL/Tokens.js#L85
-  async _dropActor(actor, tokenData) {
-    // Merge Token data with the default for the Actor
-    tokenData = mergeObject(actor.data.token, tokenData, { inplace: false });
-    // Get the Token image
+  async _dropActor(actor, xPosition, yPosition, isHidden) {
+    const tokenData = actor.data.token;
+
+    tokenData.x = xPosition;
+    tokenData.y = yPosition;
+    tokenData.hidden = isHidden;
+
     if (tokenData.randomImg) {
-      let images = await actor.getTokenImages();
-      images = images.filter(
-        (i) => images.length === 1 || !(i === canvas.tokens._lastWildcard)
-      );
+      const images = await actor.getTokenImages();
       const image = images[Math.floor(Math.random() * images.length)];
-      tokenData.img = canvas.tokens._lastWildcard = image;
+      tokenData.img = image;
     }
 
-    // Submit the Token creation request and activate the Tokens layer (if not already active)
     return Token.create(tokenData);
+  }
+
+  async _handleJournalFolder(data, event) {
+    const folder = game.folders.get(data.id);
+    const entries = folder.content;
+    const topLeft = this._getTopLeft(event);
+
+    for (let entry of entries) {
+      await this._dropJournalEntry(entry, { x: topLeft[0], y: topLeft[1] });
+    }
+  }
+
+  async _dropJournalEntry(entry, entryData) {
+    return Note.create({
+      entryId: entry.id,
+      x: entryData.x,
+      y: entryData.y,
+    });
+  }
+
+  _getTopLeft(event) {
+    const t = canvas.tokens.worldTransform,
+      tx = (event.clientX - t.tx) / canvas.stage.scale.x,
+      ty = (event.clientY - t.ty) / canvas.stage.scale.y;
+
+    return canvas.grid.getTopLeft(tx, ty);
   }
 }
