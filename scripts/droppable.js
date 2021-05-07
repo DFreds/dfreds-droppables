29773c307fac320907759615390a568023a49104
Derek Fredrickson <dfreds08@gmail.com>
2021-05-06 20:22:25 -0400

    Fix dropping onto the canvas

--- scripts/droppable.js
+++ scripts/droppable.js
@@ -194,7 +194,7 @@ export default class Droppable {
   }
 
   async _dropActor(actor, xPosition, yPosition, isHidden) {
-    const tokenData = actor.data.token;
+    const tokenData = actor.data.token.toObject();
 
     tokenData.x = xPosition;
     tokenData.y = yPosition;
