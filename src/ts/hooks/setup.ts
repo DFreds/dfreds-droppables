09a57033c4641b7d17a2c7199babe9ff5933ff40
Derek Fredrickson <dfreds08@gmail.com>
2025-08-15 10:58:52 -0400

    Revert "Simplify debug hook logic"
    This reverts commit 6ed954588bebce9b324a9cdde0b2624f79735aa1.

--- src/ts/hooks/setup.ts
+++ src/ts/hooks/setup.ts
@@ -3,7 +3,9 @@ import { Listener } from "./index.ts";
 const Setup: Listener = {
     listen(): void {
         Hooks.once("setup", async () => {
-            CONFIG.debug.hooks = BUILD_MODE === "development";
+            if (BUILD_MODE === "development") {
+                CONFIG.debug.hooks = true;
+            }
         });
     },
 };
